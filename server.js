bdf2ddd02c8690bc5fc2b825c80eef28829c389b
Adrian Parry <subparry@gmail.com>
2021-03-20 13:55:43 -0300

    fix socket server

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const app = require('express')()
+const cors = require('cors')
 app.use(cors())
 const httpServer = require('http').createServer(app)
-const cors = require('cors')
 const io = require('socket.io')(httpServer, {
-  // ...
+  path: '/chat',
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'OPTIONS'],
+  },
 })
 
 io.on('connection', (socket) => {
-  console.log('Connection from id = ', socket.id)
+  socket.on('join-room', (roomName) => {
+    socket.join(roomName)
+  })
+
+  socket.on('send-message', (msg) => {
+    io.sockets.in('General').emit('message-echo', msg)
+  })
 })
 
 httpServer.listen(4444, () => {
